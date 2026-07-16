5dcf4917b86a12ac91b549375af07a845f1c8346
Aswin K S <ksaswin808@gmail.com>
2026-07-16 19:00:41 +0530

    fuck yeah

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ur@rt(*pn2)#yp_zk-28rb$^0mc(74a5b2+ijj(*@6##fr2pwp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["razorpay-test.duckdns.org"]
 
 
 # Application definition
